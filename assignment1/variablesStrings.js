99fdea49d1c19903382b0c6fdb5e60a3e2538b41
shafiqurrehman <shafiq.12@live.com>
2017-10-20 17:12:55 +0500

    assignment1 commited successfully

---
+++ assignment1/variablesStrings.js
@@ -0,0 +1,11 @@
+var username;
+var myName = "Wayn Rooney";
+var message;
+message = "Hello World";
+alert(message);
+
+var student = "Ali Reza";
+var roll_no = "FA17MSCS0080";
+var grade = 10;
+
+alert("Student's Name: " + student + "\n" + "Roll No: " + roll_no + "\n" + "Grade: " + grade);
