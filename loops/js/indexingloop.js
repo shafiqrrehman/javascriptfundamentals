8a4750a15af1fa16388e96d94c2c2e552c06e363
shafiqurrehman <shafiq.12@live.com>
2017-10-26 17:33:59 +0500

    fruit indexing done

---
+++ loops/js/indexingloop.js
@@ -0,0 +1,11 @@
+var arr = ["apple", "banana", "mango", "orange", "strawberry"];
+
+
+
+for (var index = 0; index < arr.length; index++) {
+    document.write(arr[index] + "<br>");
+}
+
+for (var index = 0; index < arr.length; index++) {
+    document.write("Element at index " + index + " is " + arr[index] + "<br>");
+}
