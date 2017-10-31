d89433823c36ccd141f1074364819d1473609fdc
shafiqurrehman <shafiq.12@live.com>
2017-10-31 14:22:03 +0500

    added file

---
+++ string/js/lastusr.js
@@ -0,0 +1,5 @@
+let usr = prompt("Enter your favrt country name?");
+let last = usr.charAt(usr.length - 1);
+
+document.write("User Input: " + usr + "<br>");
+document.write("Last index of user input is: " + last);
---
+++ string/js/split.js
@@ -0,0 +1,6 @@
+var university = "University of Karachi";
+var result = university.split("");
+
+for (var i = 0; i < result.length; i++) {
+    document.write(result[i] + "<br>");
+}
