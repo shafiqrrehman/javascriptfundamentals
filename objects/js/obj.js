1b2eee8fd66bdfd46a9540800eebf91175719f6d
shafiqurrehman <shafiq.12@live.com>
2017-11-14 11:42:43 +0500

    object dirctory commit

---
+++ objects/js/obj.js
@@ -0,0 +1,10 @@
+let plan1 = {
+    name: "Professional",
+    price: 48,
+    space: 100,
+    transfer: 1000,
+    pages: 10
+};
+
+
+document.write("The cost of " + plan1.name + " package is $" + plan1.price + " per month.");
