3a05999612ee7022b4c4aafd4f581cf99e0c3be0
shafiqurrehman <shafiq.12@live.com>
2018-01-16 22:41:15 +0500

    arrow function added successfully

---
+++ functions/functions-es6/arrow.js
@@ -0,0 +1,5 @@
+const volumeOfSphere = diameter => {
+  return (1/6) * Math.PI * diameter * diameter * diameter;
+}
+
+console.log('The volume of a sphere is ' + volumeOfSphere(10) + ' cubic centimeters');
