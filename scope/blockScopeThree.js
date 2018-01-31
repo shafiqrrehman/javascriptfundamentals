a1b48d642c2bbc013950adb0b4876de12a028013
shafiqurrehman <shafiq.12@live.com>
2018-01-31 15:40:38 +0500

    Defining Block Scope

---
+++ scope/blockScopeThree.js
@@ -0,0 +1,12 @@
+const starCount = () => {
+  let i = 5;
+  console.log(i); // 5
+
+  for(let i = 0; i < 12; i++) {
+    console.log(i); // log the values 0 to 11
+  }
+}
+
+starCount();
+
+console.log(i); // Error
